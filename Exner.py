d404bde995f43237a2934bc64577af010a8c1173
obeaulieu <beaul068@umn.edu>
2017-05-02 15:57:26 -0500

    Update Exner.py

--- Exner.py
+++ Exner.py
@@ -12,13 +12,17 @@ from matplotlib import pyplot as plt
 #exner Equation model 
 
 #Variables
-n = 1. * np.arange(100) #bed elevation relative to some fixed datum
-n[0] = 1000.
-n[-1] = 0.
-dt = 1E4 * 3.15E7 #time (years)
+n = 1. * np.arange(1000, -1, -10) #bed elevation relative to some fixed datum
+dt = .1 #time (seconds)
 e = 0.6 #bed porosity
-x = 1000. * np.arange(100) #downstream distance (100km)
+x = 1000. * np.arange(len(n)) #downstream distance (100km)
+dx = np.mean(np.diff(x)) # because it is regularly-spaced
 
+# Boundary conditions: Dirichlet -- already set in input
+n[0] = 995.
+n[-1] = 0.
+
+# Constants
 rho_s = 2.65
 rho_f = 1.
 D50 = 0.05
@@ -30,15 +34,23 @@ h = 100
 #fig = plt.figure()
 #plt.plot(x, n, 'r-', linewidth=2)
 
+plt.plot( x, n, 'k-', linewidth=2)
 # Run for 100 time steps
-for i in range(int(10000)):
+for i in range(int(10)):
     dn = np.diff(n)
-    dx = np.diff(x)
-    qs = 4 * (((rho_f * g * h * (dn / dx)) - (0.495 * (rho_s - rho_f) * g * D50)) / (((rho_s / rho_f) * g)) ** 1.5) * (((rho_s / rho_f) - 1) * g) #sediment flux
+    #dx = np.diff(x)
+    tau_b = (rho_f * g * h * (-dn / dx))
+    tau_c = 0.0495 * (rho_s - rho_f) * g * D50
+    qs = 4 * (tau_b - tau_c \
+         / (((rho_s / rho_f) * g)))** 1.5 * \
+         (((rho_s / rho_f) - 1) * g) #sediment flux
+    qs[tau_b < tau_c] = 0 # no sed flux
     dqs = np.diff(qs)
-    dn_dt = dqs / dx
+    dn_dt = -1/e * dqs / dx
+    n[1:-1] = n[1:-1] + dn_dt * dt
 
 
-plt.plot(dx, qs, 'b-', linewidth=2)
+#plt.plot( (x[:-1] + x[1:])/2., qs, 'b-', linewidth=2)
+plt.plot( x, n, 'b-', linewidth=2)
 
 plt.show()
